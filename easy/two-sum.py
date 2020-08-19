e08d24fab222c4f9f7bad04ad6e71e93f43461e4
virtuoso98 <58349087+virtuoso98@users.noreply.github.com>
2020-08-19 17:39:36 +0800

    updates

---
+++ easy/two-sum.py
@@ -0,0 +1,24 @@
+# acceptable, but O(n^2) time complexity
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums), 1):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                
+                
+# alternative solution: using dictionaries because of quicker lookup times. O(n) time complexity
+
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+            dictionary = {}
+    
+            for i in range(len(nums)):
+                secondNumber = target - nums[i]
+                if secondNumber in dictionary.keys():
+                    secondIndex = nums.index(secondNumber)
+                    if (i != secondIndex): 
+                        return sorted([i, secondIndex])
+
+                dictionary.update({nums[i]: i})
