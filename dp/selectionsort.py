f88ae01a688fa67a43bbb18d22237f25e7339bce
Zhao <e0014867@u.nus.edu>
2021-01-16 19:04:22 +0800

    updates
    updates

---
+++ dp/selectionsort.py
@@ -0,0 +1,13 @@
+def selection_sort(arr):
+    l = len(arr)
+    for i in range(0, l - 1):
+        min = i
+        for j in range(i + 1, l):
+            if arr[j] < arr[min]:
+                min = j
+        if min != i :
+            arr[i], arr[min] = arr[min], arr[i]
+    return arr
+
+print(selection_sort([4]))
+
