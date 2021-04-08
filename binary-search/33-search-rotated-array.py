769d9849cb75324dc6ff5ee490a824a46efc6f3a
virtuoso98 <e0014867@u.nus.edu>
2021-04-08 21:50:33 +0800

    finished sum of 2 integers

---
+++ binary-search/33-search-rotated-array.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] >= nums[l]:
+                if target >= nums[l] and target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if target <= nums[r] and target > nums[mid]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+        return -1
+        
